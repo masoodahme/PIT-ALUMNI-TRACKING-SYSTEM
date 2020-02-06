4abf543caaa0949115dcd3abafdc1e7b2ae8e3a9
Masood Ahmed <ayishanaznee@gmail.com>
2020-02-06 23:58:55 +0530

    Initial Commit

---
+++ CMS_TEMPLATE/includes/footer.php
@@ -0,0 +1,12 @@
+ <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2020</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
---
+++ CMS_TEMPLATE/search.php
@@ -0,0 +1,86 @@
+<?php include("includes/db.php");?>
+<!DOCTYPE html>
+<html lang="en">
+<!--HEADER -->
+<?php include("includes/header.php"); ?>
+<body>
+
+    <!-- Navigation -->
+    <?php include("includes/navigation.php"); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+               <?php
+        if(isset($_POST['submit']))
+        {
+            $search=$_POST['search'];
+            $query="select *from post where post_tags LIKE '%$search%'";
+            $result=mysqli_query($connection,$query);
+            if(mysqli_num_rows($result)==0)
+            {
+                echo"NO RESULT";
+                
+            }  
+            else
+            {
+                while($row=mysqli_fetch_assoc($result))
+                {
+                    $post_title=$row['post_title'];
+                    $post_author=$row['post_author'];
+                    $post_date=$row['post_date'];
+                    $post_image=$row['post_image'];
+                    $post_content=$row['post_content'];
+                           // echo $post_content;
+                     
+                ?>
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#"><?php echo  $post_title ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo  $post_author ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span><?php  echo  $post_date ?></p>
+                <hr>
+                <img class="img-responsive" src="images/<?php echo $post_image?>" alt="">
+                <hr>
+                <p><?php echo $post_content?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+             
+            <?php       
+                }
+            }
+        }
+            ?>
+            </div>
+            <!-- Blog Sidebar Widgets Column -->
+          <?php include("includes/sidebar.php")?>
+
+        </div>
+        <!-- /.row -->
+
+       <!--FOOTER-->
+       <?php include("includes/footer.php")?>
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
